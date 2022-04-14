55e5a665da2ef6d21c2101519c935587d95ce9e1
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2022-04-14 16:10:54 -0500

    Corrección de estilos para celular

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
 const navSlide = () => {
-    const burger = document.querySelector('.head__nav--burger');
-    const nav = document.querySelector('.head__nav--content');
-    const navLinks = document.querySelectorAll('.head__nav--content li')
+    const burger = document.querySelector('.header__nav--burger');
+    const nav = document.querySelector('.header__nav--content');
+    const navLinks = document.querySelectorAll('.header__nav--content li')
 
     burger.addEventListener('click', () => {
         //Toogle Nav
-        nav.classList.toggle('head__nav--contentActive');
+        nav.classList.toggle('header__nav--contentActive');
         
         //Animated links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `head__nav--contentFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+                link.style.animation = `header__nav--contentFade 0.5s ease forwards ${index / 7 + 0.2}s`;
             }
         });
         //Burger Animation
