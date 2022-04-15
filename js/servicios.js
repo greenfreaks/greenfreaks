bf067906c0191a2402362f9e22d9689797690871
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2022-04-15 13:26:41 -0500

    Slider general sirve corretamente

---
+++ js/servicios.js
@@ -0,0 +1,27 @@
+    let showWeb = document.querySelector('.showWeb');
+    showWeb.classList.add('active');
+    let showAplicaciones = document.querySelector('.showAplicaciones');
+
+    let sliderWeb = document.querySelector('.slider__web');
+    let sliderAplicaciones = document.querySelector('.slider__aplicaciones');
+
+    sliderWeb.style.display = "block";
+    sliderAplicaciones.style.display = "none";
+
+    showWeb.addEventListener("click", showSliderWeb);
+    showAplicaciones.addEventListener("click", showSliderAplicaciones);
+
+    function showSliderWeb(){
+        sliderWeb.style.display = "block";
+        sliderAplicaciones.style.display = "none";
+
+        showWeb.classList.add('active');
+        showAplicaciones.classList.remove('active');
+    }
+    function showSliderAplicaciones(){
+        sliderWeb.style.display = "none";
+        showWeb.classList.remove('active');
+
+        sliderAplicaciones.style.display = "block";
+        showAplicaciones.classList.add('active');
+    }
